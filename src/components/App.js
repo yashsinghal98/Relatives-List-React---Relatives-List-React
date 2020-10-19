b14a267973207b255e8a59d8dcd435e2b03f6680
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-19 19:18:51 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    relatives=["yash","harsh","vipul","praful"];
     render() {
         let relatives=["yash","harsh","vipul","praful"];
         return(
